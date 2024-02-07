bc161c542501fe15e84a6db094dfcd1f37297bbf
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2024-02-07 23:18:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // write js code here if required
+let timeContent = document.getElementById("timer");
+let time = new Date();
+setInterval(() => {
+	time = new Date();
+	timeContent.innerText = `${time}`;
+},1000)
+//console.log(time);
+
